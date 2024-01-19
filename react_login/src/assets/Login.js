d0693b5839d04a2c18934957bf7a19872ddc51f0
Brian12345-webdev <132722257+Brian12345-webdev@users.noreply.github.com>
2024-01-19 08:36:45 -0600

    update to app.js, and login.js

---
+++ react_login/src/assets/Login.js
@@ -0,0 +1,26 @@
+import { useRef, useState, useEffect } from 'react';
+
+const Login  = () => {
+    const userRef = useRef();
+    const errRef = useRef();
+
+    const [user, setUser] = useState('');
+    const [pwd, setPwd] = useState('');
+    const[errMsg, setErrMsg] = useState('');
+    const [success, setSuccess] = useState('false');
+
+    useEffect(() => {
+        userRef.current.focus();
+    }, [])
+
+    useEffect(() => {
+        setErrMsg('');
+    }, [user, pwd])
+
+    return (
+        <section>
+            <p ref={errRef} className={errMsg}></p>
+        </section>
+    )
+}
+export default Login;
